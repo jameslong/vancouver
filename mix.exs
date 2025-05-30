6ef4affbc12cd1e4ca08e339f57f7ddcc184dd5d
jameslong <846867+jameslong@users.noreply.github.com>
2025-05-30 14:38:47 -0700

    Bump version from 0.1.0 -> 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Vancouver.MixProject do
   def project do
     [
       app: :vancouver,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       description: description(),
       package: package(),
