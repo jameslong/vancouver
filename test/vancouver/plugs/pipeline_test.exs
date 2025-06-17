91cb7f0fd55568ada0ff8c24a9c60377d4804e35
jameslong <846867+jameslong@users.noreply.github.com>
2025-06-17 15:27:09 -0700

    Add extra integration tests

--- test/vancouver/plugs/pipeline_test.exs
+++ test/vancouver/plugs/pipeline_test.exs
@@ -1,11 +1,17 @@
 defmodule Vancouver.Plugs.PipelineTest do
   use ExUnit.Case, async: true
 
+  import Plug.Conn
   import Vancouver.TestHelpers
+
   alias Vancouver.Plugs.Pipeline
+  alias Vancouver.Prompts
+  alias Vancouver.PromptTest
+  alias Vancouver.Tools
+  alias Vancouver.ToolTest
 
   describe "call/2" do
-    test "with valid json rpc2 request returns success" do
+    test "with valid MCP request (ping) returns success" do
       conn =
         ping_request()
         |> request_conn()
@@ -14,7 +20,29 @@ defmodule Vancouver.Plugs.PipelineTest do
       assert_success(conn)
     end
 
-    test "with invalid json rpc2 request returns error" do
+    test "with valid MCP request (tool call) returns success" do
+      conn =
+        tools_call_request("calculate_sum", %{"a" => 1, "b" => 2})
+        |> request_conn()
+        |> assign(:vancouver, %{tools: [Tools.CalculateSum]})
+        |> Pipeline.call([])
+
+      assert_success(conn)
+      assert ToolTest.text_response(conn)
+    end
+
+    test "with valid MCP request (prompt get) returns success" do
+      conn =
+        prompts_get_request("code_review", %{"code" => "def hello, do: \"Hello!\""})
+        |> request_conn()
+        |> assign(:vancouver, %{prompts: [Prompts.CodeReview]})
+        |> Pipeline.call([])
+
+      assert_success(conn)
+      assert PromptTest.text_response(conn)
+    end
+
+    test "with invalid MCP request (invalid json rpc) returns error" do
       conn =
         %{ping_request() | "jsonrpc" => "1.0"}
         |> request_conn()
